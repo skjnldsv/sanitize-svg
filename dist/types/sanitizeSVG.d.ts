22fb9807bf2ca3a28b09f0a0ac53ed3691496617
John Molakvoæ <skjnldsv@protonmail.com>
2022-06-17 08:26:54 +0200

    Npm usage
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- dist/types/sanitizeSVG.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-declare const sanitizeSVG: (svg: File | Buffer, window?: Window | null) => Promise<File | Buffer | null>;
-export default sanitizeSVG;
-//# sourceMappingURL=sanitizeSVG.d.ts.map
--- dist/types/server.d.ts
+++
@@ -1,3 +0,0 @@
-declare const client: (svg: File, window?: Window | null) => Promise<File>;
-export default client;
-//# sourceMappingURL=server.d.ts.map
